0a272138b0141c2a3f46df1dcdc56c9dbed06e4f
Fabian Dill <fabian.dill@web.de>
2022-07-04 00:38:27 +0200

    Add Alchemy Protected Cards bypass

--- Plugin.cs
+++ Plugin.cs
@@ -13,10 +13,11 @@ namespace SpaceTravelIdleUnlocker
     {
         public static Plugin Instance;
         public static ScientificNotation SciNoResourceGainMultiplier;
-        public static Dictionary<BonusType, ConfigEntry<double>> BonusDict = new ();
-        public static Dictionary<BonusType, ConfigEntry<double>> AddBonusDict = new ();
+        public static Dictionary<BonusType, ConfigEntry<double>> BonusDict = new();
+        public static Dictionary<BonusType, ConfigEntry<double>> AddBonusDict = new();
+        public static ConfigEntry<int> ExtraEquipSlots;
 
-        public static HashSet<BonusType> MulBonusTypes = new HashSet<BonusType> ()
+        public static HashSet<BonusType> MulBonusTypes = new HashSet<BonusType>()
         {
             BonusType.erDeteriorationSlowdown,
             BonusType.erDeteriorationMinBorderUp,
@@ -54,7 +55,7 @@ namespace SpaceTravelIdleUnlocker
             BonusType.prodResourceCostReduction
         };
 
-        public static HashSet<BonusType> AddBonusTypes = new HashSet<BonusType> ()
+        public static HashSet<BonusType> AddBonusTypes = new HashSet<BonusType>()
         {
             BonusType.permAtk,
             BonusType.permDef,
@@ -70,6 +71,7 @@ namespace SpaceTravelIdleUnlocker
         public ConfigEntry<double> DarkMatterFactor;
         public ConfigEntry<bool> GrantBetaCards;
         public ConfigEntry<double> ResourceGainMultiplier;
+        public ConfigEntry<bool> AllowProtectedAlchemy;
 
         private void Awake()
         {
@@ -79,25 +81,35 @@ namespace SpaceTravelIdleUnlocker
             ResourceGainMultiplier = Config.Bind("General", "ResourceGainMultiplier", 1.0,
                 "Multiplies all resource gains by this factor.");
             SciNoResourceGainMultiplier = new ScientificNotation(ResourceGainMultiplier.Value);
-            
+            // Cards
             GrantBetaCards = Config.Bind("General", "GrantBetaCards", false,
                 "Grants the user a set of cards that is normally limited to beta testers. Seems to require a new save game.");
+            AllowProtectedAlchemy = Config.Bind("General", "AllowProtectedAlchemy", false,
+                "Allows Alchemy recipes to use protected Cards when crafting.");
+            ExtraEquipSlots = Config.Bind("General", "ExtraEquipSlots", 0,
+                "Add this many Perm Card slots to be equipped.");
 
+            // Dark Matter
             DarkMatterExponent = Config.Bind("DarkMatter", "Exponent", 0.9, "Exponent component of Big Bang Formula.");
             DarkMatterFactor = Config.Bind("DarkMatter", "Factor", 0.35, "Factor component of Big Bang Formula.");
-            
+
+            // Bonus
             foreach (BonusType bonus in MulBonusTypes)
             {
-                BonusDict[bonus] = Config.Bind("Bonus", "Mul"+bonus.ToString(),  1.0,
+                BonusDict[bonus] = Config.Bind("Bonus", "Mul" + bonus.ToString(), 1.0,
                     "Multiply bonus by this value.");
             }
+
             foreach (BonusType bonus in AddBonusTypes)
             {
-                AddBonusDict[bonus] = Config.Bind("Bonus", "Add"+bonus.ToString(),  0.0,
+                AddBonusDict[bonus] = Config.Bind("Bonus", "Add" + bonus.ToString(), 0.0,
                     "Add to bonus by this value.");
             }
+
             Harmony.CreateAndPatchAll(typeof(Plugin));
             Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is initialized!");
+            // Dump data
+            //Logger.LogInfo(Utils.ReadJsonResource("cardTemplates"));
         }
 
         // General Section
@@ -109,9 +121,61 @@ namespace SpaceTravelIdleUnlocker
             __result *= SciNoResourceGainMultiplier;
         }
 
+        [HarmonyPatch(typeof(CardBag), "HasAlchemyRecipeCards")]
+        [HarmonyPrefix]
+        static bool ModifiedHasAlchemyRecipeCards(CardAlchemyRecipe recipe, ref bool __result, CardBag __instance)
+        {
+            if (!Instance.AllowProtectedAlchemy.Value)
+            {
+                return true;
+            }
+
+            foreach (string cardIdLevelCountStr in recipe.fromCardIdLevelCountStrings)
+            {
+                Card card;
+                int num;
+                CardAlchemyRecipe.ProcessCardIdLevelCountStr(cardIdLevelCountStr, out card, out num);
+                if (__instance.battleCardsDict.ContainsKey(card.id) &&
+                    __instance.battleCardsDict[card.id].levelDict.ContainsKey(card.level))
+                {
+                    if (__instance.battleCardsDict[card.id].levelDict[card.level].count -
+                        __instance.CountInLoadout(card.id, card.level) < num)
+                    {
+                        __result = false;
+                        return false;
+                    }
+                }
+                else
+                {
+                    if (!__instance.permanentCardsDict.ContainsKey(card.id) ||
+                        !__instance.permanentCardsDict[card.id].levelDict.ContainsKey(card.level))
+                    {
+                        __result = false;
+                        return false;
+                    }
+
+                    if (__instance.permanentCardsDict[card.id].levelDict[card.level].count -
+                        __instance.CountInLoadout(card.id, card.level) < num)
+                    {
+                        __result = false;
+                        return false;
+                    }
+                }
+            }
+
+            __result = true;
+            return false; // Returning false in prefix patches skips running the original code
+        }
+
+        [HarmonyPatch(typeof(Modifiers), "extraEquipSlot", MethodType.Getter)]
+        [HarmonyPostfix]
+        static void ModifiedGetAddBonus(ref int __result)
+        {
+            __result += ExtraEquipSlots.Value;
+        }
+
         [HarmonyPatch(typeof(Modifiers), "GetAddBonus")]
         [HarmonyPostfix]
-        
         static void ModifiedGetAddBonus(BonusType type, ref ScientificNotation __result)
         {
             if (AddBonusTypes.Contains(type))
@@ -148,11 +212,14 @@ namespace SpaceTravelIdleUnlocker
         static bool ModifiedCalculateDarkMatter(BigBangManager __instance)
         {
             __instance.calculatedBigBangDarkMatter = __instance.CalculateBigBangDarkMatter();
-            __instance.calculatedTotalDarkMatter = __instance.calculatedBigBangDarkMatter + __instance.currentTotalDarkMatter;
-            __instance.calculatedDarkMatterIncrement = ((__instance.calculatedBigBangDarkMatter > ScientificNotation.zero) ? 
-                __instance.calculatedBigBangDarkMatter : ScientificNotation.zero);
+            __instance.calculatedTotalDarkMatter =
+                __instance.calculatedBigBangDarkMatter + __instance.currentTotalDarkMatter;
+            __instance.calculatedDarkMatterIncrement =
+                ((__instance.calculatedBigBangDarkMatter > ScientificNotation.zero)
+                    ? __instance.calculatedBigBangDarkMatter
+                    : ScientificNotation.zero);
 
-            
+
             return false; // Returning false in prefix patches skips running the original code
         }
 
@@ -163,9 +230,12 @@ namespace SpaceTravelIdleUnlocker
             __instance.researchScore = TechnoManager.shared.CalculateBigBangResearchScore();
             __instance.infraScore = TechnoManager.shared.CalculateBigBangInfraScore();
             ScientificNotation a = __instance.researchScore + __instance.infraScore;
-            StatsManager.shared.CalculateBigBangDMStats(out __instance.energyScore, out __instance.travelDistanceScore, out __instance.maxDamageScore);
-            ScientificNotation num = new ScientificNotation(Mathf.Max(0, __instance.energyScore + __instance.travelDistanceScore + __instance.maxDamageScore));
-            double num2 = Math.Pow((a * num).Standard(), Instance.DarkMatterExponent.Value) * Instance.DarkMatterFactor.Value;
+            StatsManager.shared.CalculateBigBangDMStats(out __instance.energyScore, out __instance.travelDistanceScore,
+                out __instance.maxDamageScore);
+            ScientificNotation num = new ScientificNotation(Mathf.Max(0,
+                __instance.energyScore + __instance.travelDistanceScore + __instance.maxDamageScore));
+            double num2 = Math.Pow((a * num).Standard(), Instance.DarkMatterExponent.Value) *
+                          Instance.DarkMatterFactor.Value;
             if (num2 <= 0.0)
             {
                 __result = ScientificNotation.zero;
@@ -174,16 +244,18 @@ namespace SpaceTravelIdleUnlocker
             {
                 __result = new ScientificNotation(num2);
             }
+
             return false;
         }
 
         [HarmonyPatch(typeof(BigBangPanel), "FixedUpdate")]
         [HarmonyPrefix]
-        static bool ModifiedTurnOnBigBangTab(BigBangPanel  __instance)
+        static bool ModifiedTurnOnBigBangTab(BigBangPanel __instance)
         {
             {
-                AccessTools.Field(typeof(BigBangPanel), "BIG_BANG_FORMULA").SetValue(__instance, 
-                    "(((R + I) * (E + T + D)) ^ " + Instance.DarkMatterExponent.Value + ") * " + Instance.DarkMatterFactor.Value + " = <color=#bf2222><b>{0}</b></color>");
+                AccessTools.Field(typeof(BigBangPanel), "BIG_BANG_FORMULA").SetValue(__instance,
+                    "(((R + I) * (E + T + D)) ^ " + Instance.DarkMatterExponent.Value + ") * " +
+                    Instance.DarkMatterFactor.Value + " = <color=#bf2222><b>{0}</b></color>");
             }
             return true;
         }
